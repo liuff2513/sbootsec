49e0cf85ffa11c5190266d350b0de82c60b40ba3
feifei.liu <feifei.liu@pcstars.com>
2017-10-28 18:34:36 +0800

    【初始调整】layui组件添加

--- src/main/java/com/sec/WebApplication.java
+++ src/main/java/com/sec/WebApplication.java
@@ -2,11 +2,34 @@ package com.sec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+
+	/**
+	 * spring boot 外部tomcat服务启动配置
+	 * @param builder
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebApplication.class);
+	}
+	@Configuration
+	public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		}
 	}
 }
